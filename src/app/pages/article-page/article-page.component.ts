ad76a5c60c76c6b142f4e5ba679ef8596706cd13
AminaChakirDev <amina.aitm@gmail.com>
2025-04-16 10:42:08 +0200

    quest 10-11 : Http - Services

--- src/app/pages/article-page/article-page.component.ts
+++ src/app/pages/article-page/article-page.component.ts
@@ -1,23 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Article } from '../../models/article.model';
+import { CommonModule } from '@angular/common';
+import { ArticleThumbnailComponent } from '../../components/article-thumbnail/article-thumbnail.component';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-article-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ArticleThumbnailComponent],
   templateUrl: './article-page.component.html',
   styleUrl: './article-page.component.scss'
 })
 export class ArticlePageComponent implements OnInit {
   articleId!: number;
+  article$! : Observable<Article>;
 
   route: ActivatedRoute = inject(ActivatedRoute);
+  apiService = inject(ApiService);
 
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.articleId = Number(params.get('id'));
-      console.log(this.articleId);
-      
+      this.article$ = this.apiService.getArticleById(this.articleId);  
     });
   }
 }
